33bce5521eb47ff585cf884cd0bc7b96461b2d32
yourmaingotoguy <khalid.n.mohamed@outlook.com>
2021-03-23 12:57:37 -0400

    Created ProjectRepository

--- ppmtool/src/main/java/com/example/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/com/example/ppmtool/domain/Project.java
@@ -1,7 +1,33 @@
 package com.example.ppmtool.domain;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date start_Date;
+    private Date end_Date;
+    private Date created_At;
+    private Date updated_At;
+
+    public Project(){
+
+    }
+
+    @PrePersist
+    protected void onCreate(){
+        this.created_At = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updated_At = new Date();
+    }
 }
