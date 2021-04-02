ac79add34074317e1a787298370924a35183d7b5
yourmaingotoguy <khalid.n.mohamed@outlook.com>
2021-04-02 13:38:43 -0400

    add postmapping create

--- ppmtool/src/main/java/com/example/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/example/ppmtool/web/ProjectController.java
@@ -2,13 +2,17 @@ package com.example.ppmtool.web;
 
 import com.example.ppmtool.domain.Project;
 import com.example.ppmtool.services.ProjectService;
+import com.fasterxml.jackson.core.JsonFactoryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -18,7 +22,15 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+        if(result.hasErrors()){
+            return new ResponseEntity<String>("Invalid project object", HttpStatus.BAD_REQUEST);
+        }
+
+        result.getFieldErrors().stream().forEach(fieldError -> {
+            String field = fieldError.getField();
+            String errorMessage = fieldError.getDefaultMessage();
+        });
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
     }
