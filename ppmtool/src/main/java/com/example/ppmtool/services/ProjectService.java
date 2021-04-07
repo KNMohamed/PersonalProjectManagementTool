a06fcfbc75789583542ffdda27edd04ad51b0326
Khalid Mohamed <khalid.tcs.mohamed@sunlife.com>
2021-04-07 17:18:18 -0400

    Get project by identifier functionality implemented

--- ppmtool/src/main/java/com/example/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/example/ppmtool/services/ProjectService.java
@@ -21,4 +21,9 @@ public class ProjectService {
         }
     }
 
+    public Project findProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if(project == null) throw new ProjectIdException("Project with ID '" + projectId + "' Does not exist");
+        return project;
+    }
 }
